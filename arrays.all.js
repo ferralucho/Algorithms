2b44725ff3248931557f31cd1eef8703c50eec25
Luciano Ferrari <lucianodarioferrari@gmail.com>
2018-03-21 23:51:45 -0300

    Add arrays all

---
+++ arrays.all.js
@@ -0,0 +1,13 @@
+// all
+
+// Returns true if the provided predicate function returns true for all elements in a collection, false otherwise.
+
+// Use Array.every() to test if all elements in the collection return true based on fn. Omit the second argument, fn, to use Boolean as a default.
+
+const all = (arr, fn = Boolean) => arr.every(fn);
+
+// Examples
+
+all([4, 2, 3], x => x > 1); // true
+all([1, 2, 3]); // true
+
