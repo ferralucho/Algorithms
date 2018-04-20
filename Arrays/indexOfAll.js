0e791b44a08224b2379bbdbc548b13493a441c53
Luciano Ferrari <lucianodarioferrari@gmail.com>
2018-04-19 21:29:43 -0300

    add index of all

---
+++ Arrays/indexOfAll.js
@@ -0,0 +1,14 @@
+// indexOfAll
+
+// Returns all indices of val in an array. If val never occurs, returns [].
+
+// Use Array.forEach() to loop over elements and Array.push() to store indices for matching elements. Return the array of indices.
+
+const indexOfAll = (arr, val) => {
+  const indices = [];
+  arr.forEach((el, i) => el === val && indices.push(i));
+  return indices;
+};
+
+indexOfAll([1, 2, 3, 1, 2, 3], 1); // [0,3]
+indexOfAll([1, 2, 3], 4); // []
