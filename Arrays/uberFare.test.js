24bfbf43a8245e3bdad59ae2e4b058b8e29664f2
Luciano Ferrari <luciano@bankuish.com>
2023-06-12 19:50:29 -0300

    uber fare

---
+++ Arrays/uberFare.test.js
@@ -0,0 +1,22 @@
+function solution(length, fares){
+    const credit = 20;
+    const rides = ["UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"]
+    let bestFare = 0;
+    let bestCar = "";
+
+    const index = fares.findLastIndex((fare, i) => {
+        console.log({i, length, fare, result: fare * length})
+        return (fare * length) <= credit
+    })
+    return index
+}
+
+const sol = solution(30, [0.3, 0.5, 0.7, 1, 1.3])
+
+console.log(sol);
+
+describe('solution', () => {
+    test('returns the correct index of the fare', () => {
+        expect(sol).toBe(1);
+    });
+});
